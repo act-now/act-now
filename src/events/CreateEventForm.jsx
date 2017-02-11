a942276e6c127b87ff935bc3c74d7858aea3dfeb
Jonah Price <jyoonah@gmail.com>
2017-02-11 14:15:00 -0800

    Add form fields

--- src/events/CreateEventForm.jsx
+++ src/events/CreateEventForm.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
+import DatePicker from 'material-ui/DatePicker';
+import TimePicker from 'material-ui/TimePicker';
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 export default () => (
   <form>
     <TextField floatingLabelText="Title" hint="Womens March" />
-    <TextField floatingLabelText="Description" multiline />
+    <TextField floatingLabelText="Description" multiLine />
+    <TextField floatingLabelText="Location" />
+    <DatePicker floatingLabelText="Date" />
+    <TimePicker floatingLabelText="Time" />
+    <RaisedButton label="Full width" fullWidth type="submit" />
   </form>
 );
