76d1738220a49b14e08bcca5383bc9725f0ea74c
Steven Bassett <steven.j.bassett@gmail.com>
2017-02-11 14:03:08 -0800

    UpdatE: add desc

--- src/events/CreateEventForm.jsx
+++ src/events/CreateEventForm.jsx
@@ -4,5 +4,6 @@ import TextField from 'material-ui/TextField';
 export default () => (
   <form>
     <TextField floatingLabelText="Title" hint="Womens March" />
+    <TextField floatingLabelText="Description" multiline />
   </form>
 );
