aebd78dc8a74a83a5cbc6943740d53d688800076
Steven Bassett <steven.j.bassett@gmail.com>
2017-02-11 16:52:21 -0800

    Update: add the renaming views

--- src/events/index.jsx
+++ src/events/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import NewEvent from './NewEvent';
-import EventDetails from './EventDetails';
+import EventView from './EventView';
 import EditEvent from './EditEvent';
 
 export default (
   <Route path="events">
     <Route path="new" component={NewEvent} />
     <Route path=":id">
-      <IndexRoute component={EventDetails} />
+      <IndexRoute component={EventView} />
       <Route path="edit" component={EditEvent} />
     </Route>
   </Route>
