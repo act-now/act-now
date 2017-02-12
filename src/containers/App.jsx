39938c574fa0cf4d721aba04d7afe497cc440827
Steven Bassett <steven.j.bassett@gmail.com>
2017-02-11 16:40:55 -0800

    update: first event edit form

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import './App.css'
 
 export default props => (
   <div>
     <AppBar title="Act Now!" />
-    {props.children}
+    <div className="app-container">
+      {props.children}
+    </div>
   </div>
 );
---
+++ src/events/redux.js
@@ -0,0 +1,57 @@
+import { createAction, handleActions } from 'redux-actions';
+
+const init = {
+  new: {
+    title: '',
+    description: '',
+    date: null,
+    location: null,
+  },
+};
+const CHANGE_TITLE = 'events/CHANGE_TITLE';
+const CHANGE_DESCRIPTION = 'events/CHANGE_DESCRIPTION';
+const CHANGE_DATE = 'events/CHANGE_DATE';
+
+export const changeTitle = createAction(CHANGE_TITLE, (id, title) => ({ id, title }));
+export const changeDescription = createAction(
+  CHANGE_DESCRIPTION,
+  (id, description) => ({ id, description }),
+);
+
+export const changeDate = createAction(
+  CHANGE_DATE,
+  (id, date) => ({ id, date }),
+);
+
+export default handleActions({
+  [CHANGE_TITLE]: (state, action) => {
+    const { payload: { id, title } } = action;
+    return {
+      ...state,
+      [id]: {
+        ...state[id],
+        title,
+      },
+    };
+  },
+  [CHANGE_DESCRIPTION]: (state, action) => {
+    const { payload: { id, description } } = action;
+    return {
+      ...state,
+      [id]: {
+        ...state[id],
+        description,
+      },
+    };
+  },
+  [CHANGE_DATE]: (state, action) => {
+    const { payload: { id, date } } = action;
+    return {
+      ...state,
+      [id]: {
+        ...state[id],
+        date,
+      },
+    };
+  },
+}, init);
