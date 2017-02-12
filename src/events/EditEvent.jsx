8d1e48ce7dea72f8b0ffe4edbe16ff422b1cc81d
Steven Bassett <steven.j.bassett@gmail.com>
2017-02-11 16:49:16 -0800

    add scafollding for edit and view

---
+++ src/events/EditEvent.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class EditEvent extends Component {
+
+  render() {
+    return <h1>Edit Event</h1>;
+  }
+}
--- src/events/index.jsx
+++ src/events/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 import NewEvent from './NewEvent';
-import EventDetails from './EventDetails.jsx';
+import EventDetails from './EventDetails';
+import EditEvent from './EditEvent';
 
 export default (
   <Route path="events">
     <Route path="new" component={NewEvent} />
-    <Route path=":id" component={EventDetails} />
+    <Route path=":id">
+      <IndexRoute component={EventDetails} />
+      <Route path="edit" component={EditEvent} />
+    </Route>
   </Route>
-)
+);
